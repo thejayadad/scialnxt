0ae8275c99ff71a6c36c2e576555369ef9f82edf
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-20 18:11:30 -0400

    update

--- myapp/app/api/user/[id]/route.js
+++ myapp/app/api/user/[id]/route.js
@@ -46,3 +46,31 @@ export async function PUT(req, ctx) {
     }
 
 }
+
+export async function DELETE(req, ctx) {
+    await db.connect()
+
+    const id = ctx.params.id
+
+    const accessToken = req.headers.get('authorization')
+    const token = accessToken.split(' ')[1]
+
+    const decodedToken = verifyJwtToken(token)
+
+    if (!accessToken || !decodedToken) {
+        return new Response(JSON.stringify({ error: "unauthorized (wrong or expired token)" }), { status: 403 })
+    }
+
+    try {
+        const user = await User.findById(id)
+        if (user?._id.toString() !== decodedToken._id.toString()) {
+            return new Response(JSON.stringify({ msg: 'Only author can delete their profile' }), { status: 403 })
+        }
+
+        await User.findByIdAndDelete(id)
+
+        return new Response(JSON.stringify({msg: 'Successfully deleted user'}), {status: 200})
+    } catch (error) {
+        return new Response(JSON.stringify(null), { status: 500 }) 
+    }
+}
