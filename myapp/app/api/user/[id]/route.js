4e36bfee5ffc7e1f5e482872d28a2f74533d982b
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-20 17:28:52 -0400

    update

--- myapp/app/api/user/[id]/route.js
+++ myapp/app/api/user/[id]/route.js
@@ -16,3 +16,33 @@ export async function GET(req, ctx) {
         return new Response(JSON.stringify(null), { status: 500 })
     }
 }
+
+export async function PUT(req, ctx) {
+    await db.connect()
+
+    const id = ctx.params.id
+    const accessToken = req.headers.get('authorization')
+    const token = accessToken.split(" ")[1]
+
+    const decodedToken = verifyJwtToken(token)
+
+    if (!accessToken || !decodedToken) {
+        return new Response(JSON.stringify({ error: "unauthorized (wrong or expired token)" }), { status: 403 })
+    }
+
+    try {
+        const body = await req.json()
+        const user = await User.findById(id)
+
+        if (user?._id.toString() !== decodedToken._id.toString()) {
+            return new Response(JSON.stringify({ msg: 'Only author can update their profile' }), { status: 403 })
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, { $set: { ...body } }, { new: true })
+
+        return new Response(JSON.stringify(updatedUser), { status: 200 })
+    } catch (error) {
+        return new Response(JSON.stringify(null), { status: 500 })
+    }
+
+}
---
+++ myapp/components/UserCard.js
@@ -0,0 +1,16 @@
+'use client'
+
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react'
+
+
+const UserCard = () => {
+  return (
+   <div>
+
+   </div>
+  )
+}
+
+export default UserCard
