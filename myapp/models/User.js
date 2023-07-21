0e03a016e75fe667ecce58142d510c37b77665c8
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-21 11:56:29 -0400

    update

--- myapp/models/User.js
+++ myapp/models/User.js
@@ -1,40 +1,45 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-   username: {
-    type: String,
-    required: true,
-    unique: true,
-   },
-   email: {
-    type: String,
-    required: true,
-    unique: true,
-   },
-   password: {
-    type: String,
-    required: true,
-   },
-   profilePic: {
-    type: String,
-    default: ''
-   },
-   coverPic: {
-    type: String,
-    default: ''
-   },
-   followers: {
-    type: Array,
-    default: []
-   },
-   followings: {
-    type: Array,
-    default: []
-   },
-   posts: {
-    type: Array,
-    default: []
-   }
-}, {timestamps: true})
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
 
-export default mongoose?.models?.User || mongoose.model("User", UserSchema)
+const User = mongoose.model("User", UserSchema);
+export default User;
